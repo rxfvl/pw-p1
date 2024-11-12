ef0af5a576e25ca8742a9c9926fc6bc6cc00ad45
rxfvl <minecraftiano024@gmail.com>
2024-11-12 16:51:39 +0100

    mysqlconnector

--- practica2/src/es/uco/practica2/data/common/DBConnection.java
+++ practica2/src/es/uco/practica2/data/common/DBConnection.java
@@ -1,5 +1,9 @@
 package es.uco.practica2.data.common;
 
-public class DBConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DBConnection {
+	
 }
