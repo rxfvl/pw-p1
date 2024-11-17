2f9a050b4516036184096ff3ccb1d9163b85a0a1
MiriamPM26 <152272951+MiriamPM26@users.noreply.github.com>
2024-11-17 11:36:11 +0100

    Update MainJugadores.java

--- practica2/src/es/uco/practica2/display/MainJugadores.java
+++ practica2/src/es/uco/practica2/display/MainJugadores.java
@@ -9,109 +9,121 @@ import java.util.Properties;
 import java.util.Scanner;
 import es.uco.practica2.business.JugadorDTO;
 import es.uco.practica2.business.GestorJugadores;
+import es.uco.practica2.data.common.DBConnection;
 
 public class MainJugadores {
     public static void main(String[] args) {
         Properties propiedades = new Properties();
-        Connection connection = new DBConnection().getConnection(); // Conexión a la base de datos
-        GestorJugadores gestorJugadores = new GestorJugadores(connection);
+        Connection connection = null;
 
-        // Cargar propiedades del fichero
-        try (InputStream input = new FileInputStream("config.properties")) {
-            propiedades.load(input);
-        } catch (IOException ex) {
-            System.out.println("Error al cargar las propiedades: " + ex.getMessage());
-            return;
-        }
+        try {
+            connection = new DBConnection().getConnection(); // Conexión a la base de datos
+            GestorJugadores gestorJugadores = new GestorJugadores(connection);
 
-        boolean salir = false;
-        Scanner scanner = new Scanner(System.in);
+            // Cargar propiedades del fichero
+            try (InputStream input = new FileInputStream("config.properties")) {
+                propiedades.load(input);
+            } catch (IOException ex) {
+                System.out.println("Error al cargar las propiedades: " + ex.getMessage());
+                return;
+            }
 
-        while (!salir) {
-            System.out.println("1. Añadir Jugador");
-            System.out.println("2. Modificar información Jugador");
-            System.out.println("3. Listar Jugadores");
-            System.out.println("0. Salir");
-            System.out.print("Selecciona una opción: ");
-            int opcion = scanner.nextInt();
-            scanner.nextLine(); // Limpiar el buffer
+            boolean salir = false;
+            Scanner scanner = new Scanner(System.in);
 
-            switch (opcion) {
-                case 1:
-                    // Lógica para añadir un jugador
-                    System.out.print("Ingrese su nombre: ");
-                    String nombre = scanner.nextLine();
+            while (!salir) {
+                System.out.println("1. Añadir Jugador");
+                System.out.println("2. Modificar información Jugador");
+                System.out.println("3. Listar Jugadores");
+                System.out.println("0. Salir");
+                System.out.print("Selecciona una opción: ");
+                int opcion = scanner.nextInt();
+                scanner.nextLine(); // Limpiar el buffer
 
-                    System.out.print("Ingrese sus apellidos: ");
-                    String apellidos = scanner.nextLine();
+                switch (opcion) {
+                    case 1:
+                        // Lógica para añadir un jugador
+                        System.out.print("Ingrese su nombre: ");
+                        String nombre = scanner.nextLine();
 
-                    System.out.print("Ingrese su correo electrónico: ");
-                    String correo = scanner.nextLine();
+                        System.out.print("Ingrese sus apellidos: ");
+                        String apellidos = scanner.nextLine();
 
-                    System.out.print("Ingrese su fecha de nacimiento (dd/MM/yyyy): ");
-                    String fechaNac = scanner.nextLine();
-                    Date fechaNacimiento = new SimpleDateFormat("dd/MM/yyyy").parse(fechaNac);  // Asegúrate de manejar este parse correctamente
+                        System.out.print("Ingrese su correo electrónico: ");
+                        String correo = scanner.nextLine();
 
-                    System.out.print("Ingrese la fecha de inscripción (dd/MM/yyyy): ");
-                    String fechaInscripcion = scanner.nextLine();
-                    Date fechaIns = new SimpleDateFormat("dd/MM/yyyy").parse(fechaInscripcion); // Asegúrate de manejar este parse correctamente
+                        System.out.print("Ingrese su fecha de nacimiento (dd/MM/yyyy): ");
+                        String fechaNac = scanner.nextLine();
+                        Date fechaNacimiento = new SimpleDateFormat("dd/MM/yyyy").parse(fechaNac);  // Asegúrate de manejar este parse correctamente
 
-                    JugadorDTO jugador = new JugadorDTO(0, nombre, apellidos, fechaNacimiento, fechaIns, correo); // ID inicial en 0
+                        System.out.print("Ingrese la fecha de inscripción (dd/MM/yyyy): ");
+                        String fechaInscripcion = scanner.nextLine();
+                        Date fechaIns = new SimpleDateFormat("dd/MM/yyyy").parse(fechaInscripcion); // Asegúrate de manejar este parse correctamente
 
-                    try {
-                        gestorJugadores.addJugador(jugador);
-                        System.out.println("Jugador añadido con éxito.");
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                case 2:
-                    // Lógica para modificar un jugador
-                    System.out.print("Introduzca el ID del jugador a modificar: ");
-                    int idM = scanner.nextInt();
-                    scanner.nextLine(); // Limpiar el buffer
-                    JugadorDTO jugadorParaModificar = gestorJugadores.getJugador(idM);
-
-                    if (jugadorParaModificar != null) {
-                        System.out.print("Nombre (actual " + jugadorParaModificar.getNombre() + "): ");
-                        jugadorParaModificar.setNombre(scanner.nextLine());
-
-                        System.out.print("Apellidos (actual " + jugadorParaModificar.getApellidos() + "): ");
-                        jugadorParaModificar.setApellidos(scanner.nextLine());
-
-                        System.out.print("Correo electrónico (actual " + jugadorParaModificar.getCorreo_electronico() + "): ");
-                        jugadorParaModificar.setCorreo_electronico(scanner.nextLine());
-
-                        // Modificar la fecha de nacimiento o inscripción si deseas
-                        // ...
+                        JugadorDTO jugador = new JugadorDTO(0, nombre, apellidos, fechaNacimiento, fechaIns, correo); // ID inicial en 0
 
                         try {
-                            gestorJugadores.updateJugador(jugadorParaModificar);
-                            System.out.println("Jugador modificado con éxito.");
+                            gestorJugadores.addJugador(jugador);
+                            System.out.println("Jugador añadido con éxito.");
                         } catch (SQLException e) {
                             e.printStackTrace();
                         }
-                    } else {
-                        System.out.println("Jugador no encontrado.");
-                    }
-                    break;
-                case 3:
-                    // Listar jugadores
-                    try {
-                        for (JugadorDTO j : gestorJugadores.getAllJugadores()) {
-                            System.out.println(j);
+                        break;
+                    case 2:
+                        // Lógica para modificar un jugador
+                        System.out.print("Introduzca el ID del jugador a modificar: ");
+                        int idM = scanner.nextInt();
+                        scanner.nextLine(); // Limpiar el buffer
+                        JugadorDTO jugadorParaModificar = gestorJugadores.getJugador(idM);
+
+                        if (jugadorParaModificar != null) {
+                            System.out.print("Nombre (actual " + jugadorParaModificar.getNombre() + "): ");
+                            jugadorParaModificar.setNombre(scanner.nextLine());
+
+                            System.out.print("Apellidos (actual " + jugadorParaModificar.getApellidos() + "): ");
+                            jugadorParaModificar.setApellidos(scanner.nextLine());
+
+                            System.out.print("Correo electrónico (actual " + jugadorParaModificar.getCorreo_electronico() + "): ");
+                            jugadorParaModificar.setCorreo_electronico(scanner.nextLine());
+
+                            try {
+                                gestorJugadores.updateJugador(jugadorParaModificar);
+                                System.out.println("Jugador modificado con éxito.");
+                            } catch (SQLException e) {
+                                e.printStackTrace();
+                            }
+                        } else {
+                            System.out.println("Jugador no encontrado.");
                         }
-                    } catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                case 0:
-                    salir = true; // Salir del bucle
-                    break;
-                default:
-                    System.out.println("Opción no válida. Por favor intenta de nuevo.");
+                        break;
+                    case 3:
+                        // Listar jugadores
+                        try {
+                            for (JugadorDTO j : gestorJugadores.getAllJugadores()) {
+                                System.out.println(j);
+                            }
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                        break;
+                    case 0:
+                        salir = true; // Salir del bucle
+                        break;
+                    default:
+                        System.out.println("Opción no válida. Por favor intenta de nuevo.");
+                }
+            }
+            scanner.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
             }
         }
-        scanner.close();
     }
 }
