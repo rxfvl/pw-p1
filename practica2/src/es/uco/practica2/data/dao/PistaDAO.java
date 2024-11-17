3c75db6d2687727f2c7cf8f598bde219631bc803
rxfvl <minecraftiano024@gmail.com>
2024-11-17 16:19:14 +0100

    Pasado a sql.properties

--- practica2/src/es/uco/practica2/data/dao/PistaDAO.java
+++ practica2/src/es/uco/practica2/data/dao/PistaDAO.java
@@ -1,13 +1,31 @@
 package es.uco.practica2.data.dao;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.*;
 //import com.mysql.jdbc.ResultSet;
 import es.uco.practica2.business.*;
 import es.uco.practica2.data.common.DBConnection;
 import java.util.List;
+import java.util.Properties;
 import java.util.ArrayList;
 
 public class PistaDAO {
+	
+	private Properties propiedades = new Properties();
+	
+		public PistaDAO()
+		{
+			try (InputStream input = new FileInputStream("sql.properties")) 
+			{
+				this.propiedades.load(input);
+			} catch (IOException ex) 
+			{
+				System.out.println("Error al cargar las propiedades: " + ex.getMessage());
+				return;
+		}
+	}
 	public int crearPista(PistaDTO pista)
 	{
 		int cont = 0, status = -1;
@@ -15,13 +33,13 @@ public class PistaDAO {
 			DBConnection dbConnection = new DBConnection();
 			Connection con = dbConnection.getConnection();
 			
-			PreparedStatement stmt = con.prepareStatement("select * from pistas where nombre = ?");
+			PreparedStatement stmt = con.prepareStatement(this.propiedades.getProperty("crearPistSelect"));
 			stmt.setString(1, pista.getNombre());
 			ResultSet rs = stmt.executeQuery();
 			
 			if(!rs.next())
 			{
-				PreparedStatement ps = con.prepareStatement("insert into pistas (nombre,estado,tipo,tamanio,jugadores_max) values(?,?,?,?,?)");
+				PreparedStatement ps = con.prepareStatement(this.propiedades.getProperty("crearPistInsert"));
 				ps.setString(1,pista.getNombre());	
 				ps.setInt(2, pista.getEstado());
 				ps.setInt(3, pista.getTipo());
@@ -49,7 +67,7 @@ public class PistaDAO {
 			DBConnection dbConnection = new DBConnection();
 			Connection con = dbConnection.getConnection();
 			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("select * from pistas");
+			ResultSet rs = stmt.executeQuery(this.propiedades.getProperty("listPistSelect"));
 			
 			while(rs.next())
 			{
@@ -81,7 +99,7 @@ public class PistaDAO {
 			DBConnection dbConnection = new DBConnection();
 			Connection con = dbConnection.getConnection();
 				
-			PreparedStatement ps = con.prepareStatement("delete from pistas where nombre=?");
+			PreparedStatement ps = con.prepareStatement(this.propiedades.getProperty("borrarPistDelete"));
 			ps.setString(1,pista.getNombre());
 				
 			status=ps.executeUpdate();
