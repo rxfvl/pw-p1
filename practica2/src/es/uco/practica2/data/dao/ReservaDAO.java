e103ab4116e429542c737fc8d264f9bd54e57a2a
MiriamPM26 <152272951+MiriamPM26@users.noreply.github.com>
2024-11-18 19:27:21 +0100

    ReservaDAO.java comentado

--- practica2/src/es/uco/practica2/data/dao/ReservaDAO.java
+++ practica2/src/es/uco/practica2/data/dao/ReservaDAO.java
@@ -17,38 +17,51 @@ import java.util.Properties;
 import es.uco.practica2.business.*;
 import es.uco.practica2.data.common.DBConnection;
 
+/**
+ * Clase ReservaDAO.
+ * 
+ * Esta clase gestiona las reservas en la base de datos. Proporciona métodos 
+ * para crear, modificar, cancelar y listar reservas. La clase también maneja 
+ * la conexión con la base de datos y las operaciones relacionadas a las reservas.
+ */
 public class ReservaDAO {
 	
 	private Properties propiedades = new Properties();
 	
-	public ReservaDAO()
-	{
-		try (InputStream input = new FileInputStream("sql.properties")) 
-		{
+	/**
+	 * Constructor de ReservaDAO.
+	 * 
+	 * Carga las propiedades necesarias para realizar las consultas SQL desde 
+	 * un archivo de configuración.
+	 */
+	public ReservaDAO() {
+		try (InputStream input = new FileInputStream("sql.properties")) {
 			this.propiedades.load(input);
-		} catch (IOException ex) 
-		{
+		} catch (IOException ex) {
 			System.out.println("Error al cargar las propiedades: " + ex.getMessage());
 			return;
 		}
 	}
 	
-	public void crearReservaIndividual(ReservasDTO reserva, String correo)
-	{
-		try{
+	/**
+	 * Crea una reserva individual.
+	 * 
+	 * @param reserva Objeto ReservasDTO que contiene la información de la reserva.
+	 * @param correo Email del usuario que realiza la reserva.
+	 */
+	public void crearReservaIndividual(ReservasDTO reserva, String correo) {
+		try {
 			DBConnection dbConnection = new DBConnection();
 			Connection con = dbConnection.getConnection();
 			PreparedStatement ps = null;
 			ps = con.prepareStatement(this.propiedades.getProperty("crearResISelect"));
 			ps.setString(1, correo);
 			ResultSet id_jugador = ps.executeQuery();
-			if (!id_jugador.next())
-			{
+			if (!id_jugador.next()) {
 				return;
 			}
-			switch(reserva.getTipo_reserva())
-			{
-				case 0:	//adultos
+			switch (reserva.getTipo_reserva()) {
+				case 0: // adultos
 					ps = con.prepareStatement(this.propiedades.getProperty("crearResInsert"));
 					ps.setInt(1, reserva.getId());	
 					ps.setDate(2, new java.sql.Date(reserva.getFecha().getTime()));
@@ -62,9 +75,8 @@ public class ReservaDAO {
 					ps.setNull(10, Types.INTEGER);
 					ps.setInt(11, id_jugador.getInt("id"));
 					ps.executeUpdate();
-				break;
-				
-				case 1:	//infantil
+					break;
+				case 1: // infantil
 					ps = con.prepareStatement(this.propiedades.getProperty("crearResInsert"));
 					ps.setInt(1, reserva.getId());	
 					ps.setDate(2, new java.sql.Date(reserva.getFecha().getTime()));
@@ -78,9 +90,8 @@ public class ReservaDAO {
 					ps.setNull(10, Types.INTEGER);
 					ps.setInt(11, id_jugador.getInt("id"));
 					ps.executeUpdate();
-				break;
-				
-				case 2:	//familiar
+					break;
+				case 2: // familiar
 					ps = con.prepareStatement(this.propiedades.getProperty("crearResInsert"));
 					ps.setInt(1, reserva.getId());	
 					ps.setDate(2, new java.sql.Date(reserva.getFecha().getTime()));
@@ -94,34 +105,35 @@ public class ReservaDAO {
 					ps.setNull(10, Types.INTEGER);
 					ps.setInt(11, id_jugador.getInt("id"));
 					ps.executeUpdate();
-				break;
-				
+					break;
 				default:
 					System.out.println("Opción no válida.");
 			}	
-		}catch(Exception e)
-		{
+		} catch (Exception e) {
 			System.err.println(e);
 			e.printStackTrace();
 		}
 	}
 	
-	public void crearReservaBono(ReservasDTO reserva, String correo)
-	{
-		try{
+	/**
+	 * Crea una reserva utilizando un bono.
+	 * 
+	 * @param reserva Objeto ReservasDTO que contiene la información de la reserva.
+	 * @param correo Email del usuario que realiza la reserva.
+	 */
+	public void crearReservaBono(ReservasDTO reserva, String correo) {
+		try {
 			DBConnection dbConnection = new DBConnection();
 			Connection con = dbConnection.getConnection();
 			PreparedStatement ps = null;
 			ps = con.prepareStatement(this.propiedades.getProperty("crearResBSelect"));
 			ps.setString(1, correo);
 			ResultSet id_jugador = ps.executeQuery();
-			if (!id_jugador.next())
-			{
+			if (!id_jugador.next()) {
 				return;
 			}
-			switch(reserva.getTipo_reserva())
-			{
-				case 0:	//adultos
+			switch (reserva.getTipo_reserva()) {
+				case 0: // adultos
 					ps = con.prepareStatement(this.propiedades.getProperty("crearResInsert"));
 					ps.setInt(1, reserva.getId());	
 					ps.setDate(2, new java.sql.Date(reserva.getFecha().getTime()));
@@ -135,9 +147,8 @@ public class ReservaDAO {
 					ps.setInt(10, reserva.getId_bono());
 					ps.setInt(11, id_jugador.getInt("id"));
 					ps.executeUpdate();
-				break;
-				
-				case 1:	//infantil
+					break;
+				case 1: // infantil
 					ps = con.prepareStatement(this.propiedades.getProperty("crearResInsert"));
 					ps.setInt(1, reserva.getId());	
 					ps.setDate(2, new java.sql.Date(reserva.getFecha().getTime()));
@@ -151,9 +162,8 @@ public class ReservaDAO {
 					ps.setInt(10, reserva.getId_bono());
 					ps.setInt(11, id_jugador.getInt("id"));
 					ps.executeUpdate();
-				break;
-				
-				case 2:	//familiar
+					break;
+				case 2: // familiar
 					ps = con.prepareStatement(this.propiedades.getProperty("crearResInsert"));
 					ps.setInt(1, reserva.getId());	
 					ps.setDate(2, new java.sql.Date(reserva.getFecha().getTime()));
@@ -167,21 +177,23 @@ public class ReservaDAO {
 					ps.setInt(10, reserva.getId_bono());
 					ps.setInt(11, id_jugador.getInt("id"));
 					ps.executeUpdate();
-				break;
-				
+					break;
 				default:
 					System.out.println("Opción no válida.");
 			}	
-		}catch(Exception e)
-		{
+		} catch (Exception e) {
 			System.err.println(e);
 			e.printStackTrace();
 		}
 	}
 	
-	public void modificarReserva(ReservasDTO reserva)
-	{
-		try{
+	/**
+	 * Modifica una reserva existente.
+	 * 
+	 * @param reserva Objeto ReservasDTO que contiene la información actualizada de la reserva.
+	 */
+	public void modificarReserva(ReservasDTO reserva) {
+		try {
 			DBConnection dbConnection = new DBConnection();
 			Connection con = dbConnection.getConnection();
 			PreparedStatement ps = null;
@@ -189,8 +201,7 @@ public class ReservaDAO {
 			ps.setInt(1, reserva.getId_pista());
 			ps.setDate(2, new java.sql.Date(reserva.getFecha().getTime()));
 			ResultSet id = ps.executeQuery();
-			if (!id.next())
-			{
+			if (!id.next()) {
 				return;
 			}
 			ps = con.prepareStatement(this.propiedades.getProperty("modResUpdate"));
@@ -204,17 +215,19 @@ public class ReservaDAO {
 			ps.setInt(8, reserva.getNum_adultos());
 			ps.setInt(9, id.getInt("id"));
 			ps.executeUpdate();
-				
-		}catch(Exception e)
-		{
+		} catch (Exception e) {
 			System.err.println(e);
 			e.printStackTrace();
 		}
 	}
 	
-	public void cancelarReserva(ReservasDTO reserva)
-	{
-		try{
+	/**
+	 * Cancela una reserva existente.
+	 * 
+	 * @param reserva Objeto ReservasDTO que contiene la información de la reserva a cancelar.
+	 */
+	public void cancelarReserva(ReservasDTO reserva) {
+		try {
 			DBConnection dbConnection = new DBConnection();
 			Connection con = dbConnection.getConnection();
 			PreparedStatement ps = null;
@@ -222,26 +235,27 @@ public class ReservaDAO {
 			ps.setInt(1, reserva.getId_pista());
 			ps.setDate(2, new java.sql.Date(reserva.getFecha().getTime()));
 			ps.executeUpdate();
-				
-		}catch(Exception e)
-		{
+		} catch (Exception e) {
 			System.err.println(e);
 			e.printStackTrace();
 		}
 	}
 	
-	public List<ReservasDTO> listarReservasFuturas()
-	{
+	/**
+	 * Lista todas las reservas futuras.
+	 * 
+	 * @return Lista de objetos ReservasDTO que representan las reservas futuras.
+	 */
+	public List<ReservasDTO> listarReservasFuturas() {
 		List<ReservasDTO> reservas = new ArrayList<>();
 
-		try{
+		try {
 			DBConnection dbConnection = new DBConnection();
 			Connection con = dbConnection.getConnection();
 			Statement stmt = con.createStatement();
 			ResultSet rs = stmt.executeQuery(this.propiedades.getProperty("lisResSelect") + LocalDate.now());
 			
-			while(rs.next())
-			{
+			while (rs.next()) {
 				Date fecha = rs.getDate("fecha");
 				int duracion = rs.getInt("duracion");
 				int id_pista = rs.getInt("id_pista");
@@ -252,13 +266,11 @@ public class ReservaDAO {
 				int num_adultos = rs.getInt("num_adultos");
 				int id_bono = rs.getInt("id_bono");
 				int id_jugador = rs.getInt("id_jugador");
-				ReservasDTO reserva = new ReservasDTO(fecha,duracion,id_pista,precio,descuento,tipo_reserva,num_ninios,num_adultos,id_bono,id_jugador);
+				ReservasDTO reserva = new ReservasDTO(fecha, duracion, id_pista, precio, descuento, tipo_reserva, num_ninios, num_adultos, id_bono, id_jugador);
 				reservas.add(reserva);
 			}
-			if (stmt != null) {stmt.close();}
-			
-		}catch(Exception e)
-		{
+			if (stmt != null) { stmt.close(); }
+		} catch (Exception e) {
 			System.err.println(e);
 			e.printStackTrace();
 		}
@@ -266,8 +278,13 @@ public class ReservaDAO {
 		return reservas;
 	}
 	
-	public ReservasDTO consultarReserva(ReservasDTO reserva)
-	{
+	/**
+	 * Consulta una reserva específica.
+	 * 
+	 * @param reserva Objeto ReservasDTO que contiene la información de la reserva a consultar.
+	 * @return Objeto ReservasDTO actualizado con la información de la reserva consultada.
+	 */
+	public ReservasDTO consultarReserva(ReservasDTO reserva) {
 		ResultSet select;
 		try {
 			DBConnection dbcon = new DBConnection();
@@ -280,8 +297,7 @@ public class ReservaDAO {
 			ps.setDate(2, new java.sql.Date(reserva.getFecha().getTime()));
 			select = ps.executeQuery();
 			
-			if (select.next())
-			{
+			if (select.next()) {
 				reserva.setDuracion(select.getInt("duracion"));
 				reserva.setPrecio(select.getFloat("precio"));
 				reserva.setDescuento(select.getFloat("descuento"));
@@ -291,8 +307,7 @@ public class ReservaDAO {
 				reserva.setId_bono(select.getInt("id_bono"));
 				reserva.setId_jugador(select.getInt("id_jugador"));
 			}
-		}catch(Exception e)
-		{
+		} catch (Exception e) {
 			System.err.println(e);
 			e.printStackTrace();
 		}
